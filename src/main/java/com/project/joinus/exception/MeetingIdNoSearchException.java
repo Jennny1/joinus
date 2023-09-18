5aef85b85353593e4384831ac795750f1dc50b0f
Jennny1 <june9282@gmail.com>
2023-09-18 17:21:31 +0900

    "모임 신청 기능 구현"

---
+++ src/main/java/com/project/joinus/exception/MeetingIdNoSearchException.java
@@ -0,0 +1,7 @@
+package com.project.joinus.exception;
+
+public class MeetingIdNoSearchException extends RuntimeException {
+    public MeetingIdNoSearchException(String s) {
+        super(s);
+    }
+}
